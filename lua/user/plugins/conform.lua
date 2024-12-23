904907180df65b049390c482421bd5ad1425da1f
Hunter-The-Great <benlowry9@gmail.com>
2024-12-23 15:50:56 -0600

    stuff

--- lua/user/plugins/conform.lua
+++ lua/user/plugins/conform.lua
@@ -1,3 +1,5 @@
+local do_autoformat = true
+
 return {
   'stevearc/conform.nvim',
   dependencies = { 'zapling/mason-conform.nvim' },
@@ -9,14 +11,20 @@ return {
         -- python = { "isort", "black" },
         go = { 'goimports', 'gofmt' },
         -- Use a sub-list to run only the first available formatter
-        javascript = { { 'prettierd' } },
-        javascriptreact = { { 'prettierd' } },
-        typescript = { { 'prettierd' } },
-        typescriptreact = { { 'prettierd' } },
+        javascript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        astro = { 'prettierd' },
+        java = { 'google-java-format' },
+        css = { '' },
       },
       format_on_save = function(bufnr)
+        if not do_autoformat then
+          return
+        end
         -- These options will be passed to conform.format()
-        local ignore_filetypes = { 'java' }
+        local ignore_filetypes = { 'java', 'css', 'html' }
         if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
           return
         end
@@ -25,6 +33,14 @@ return {
           lsp_fallback = true,
         }
       end,
+      vim.api.nvim_create_user_command('AutoformatToggle', function()
+        do_autoformat = not do_autoformat
+        if do_autoformat then
+          print 'Autoformat enabled'
+        else
+          print 'Autoformat disabled'
+        end
+      end, { nargs = 0 }),
       vim.keymap.set('n', '<leader>ff', function()
         require('conform').format { lsp_fallback = true }
       end, { desc = 'Format buffer' }),
